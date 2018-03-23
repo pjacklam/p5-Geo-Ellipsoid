39f98a5b7fb3b59759167596a7678f93a8915c9b
Peter John Acklam <pjacklam@online.no>
2018-03-23 12:41:17 +0100

    Increment version number.

--- lib/Geo/Ellipsoid.pm
+++ lib/Geo/Ellipsoid.pm
@@ -27,11 +27,11 @@ Geo::Ellipsoid - Longitude and latitude calculations using an ellipsoid model.
 
 =head1 VERSION
 
-Version 1.14, released Sun Sep 18 2016.
+Version 1.15, not released.
 
 =cut
 
-our $VERSION = '1.14';
+our $VERSION = '1.15';
 our $DEBUG = 0;
 
 =pod
